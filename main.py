00f6e20ae3666e0208843d9cce922774edb26c1b
Ilan Eliashberg <sendm2i@gmail.com>
2025-04-05 20:46:18 -0700

    init commit

--- main.py
+++ main.py
@@ -4,9 +4,17 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
+# Ideas for Naive Python Implementation
+# have a step size (variable from 1 to 1000)
+# have an array of size 1000 that holds the current samples (and potentially
+#    one computed ahead array so you aren't behind by more than 1000 samples (or whatever the step size is)
+# Assuming you now have 2 arrays, compute correlation function
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
 
 
 # Press the green button in the gutter to run the script.
