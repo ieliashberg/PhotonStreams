6a9eb002604fd7ae1de8d79c57ef1c989c7e8164
Ilan Eliashberg <sendm2i@gmail.com>
2025-04-06 23:43:11 -0700

    printing out vals, wrong answers for sure

--- main.py
+++ main.py
@@ -16,22 +16,30 @@ if __name__ == '__main__':
     delayedStream, regularStream = load_in_streams()
     bestCoincidingCount = 0
     bestDelay = 950
+
     for i in range(950, 1051):
         tempDelayedStream = delayedStream << i  # get the shifted delayedStream by cutting of the first i
-        tempRegularStream = delayedStream >> i  # get the shifted regularStream by cutting off the last i
+        tempRegularStream = regularStream >> i  # get the shifted regularStream by cutting off the last i
+
         coincidingNum = tempDelayedStream & tempRegularStream  # and the two streams
         coincidingCount = bin(coincidingNum).count('1')  # count the number of coinciding 1s
+
         if bestCoincidingCount < coincidingCount:  # update bestDelay and bestCoincidingCount if necessary
             bestCoincidingCount = coincidingCount
             bestDelay = i
 
     surroundingArr = np.zeros(10, int)  # look at surrounding values (+-5)
-    for i in range(-5, 6):
-        if i == 0:  # if it isn't surrounding (just og val) don't include it
+    for offset in range(-5, 5):
+        if offset == 0:  # if it isn't surrounding (just og val) don't include it
             continue
-        tempDelayedStream = delayedStream << (i + bestDelay)
-        tempRegularStream = delayedStream >> (i + bestDelay)
+        currDelay = offset + bestDelay
+        tempDelayedStream = delayedStream << currDelay
+        tempRegularStream = regularStream >> currDelay
         coincidingNum = tempDelayedStream & tempRegularStream
         coincidingCount = bin(coincidingNum).count('1')
 
-        surroundingArr[5 + i] = coincidingCount  # add to surroundArr
+        surroundingArr[5 + offset] = coincidingCount  # add to surroundArr
+
+    print(bestDelay)
+    print(bestCoincidingCount)
+    print(surroundingArr)
